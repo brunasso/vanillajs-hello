5833a85e35fcc88269e161ea14cab07dd0ce0a57
brunasso <53584556+brunasso@users.noreply.github.com>
2024-07-18 16:43:00 +0000

    Finalizando proyecto js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  excuseGenerate();
 };
+
+function excuseGenerate() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let selectedExcuses = [];
+  selectedExcuses.push(who[Math.floor(Math.random() * (who.length - 1))]);
+  selectedExcuses.push(action[Math.floor(Math.random() * (action.length - 1))]);
+  selectedExcuses.push(what[Math.floor(Math.random() * (what.length - 1))]);
+  selectedExcuses.push(when[Math.floor(Math.random() * (when.length - 1))]);
+
+  printExcuses(selectedExcuses);
+}
+
+function printExcuses(excuseGenerated) {
+  let excuses = document.getElementById("excuse");
+  let nuevoHTML = "";
+  for (let i = 0; i < excuseGenerated.length; i++) {
+    nuevoHTML += excuseGenerated[i] + " ";
+  }
+  excuses.innerHTML = nuevoHTML;
+}
